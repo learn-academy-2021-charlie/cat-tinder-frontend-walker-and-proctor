a51ac7046418206ae41b8c5588ebab4d57b82f7f
WalkerTexas-coder <austinsrwalker@gmail.com>
2021-08-24 11:04:02 -0700

    testing

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # Cat Tinder Frontend
+
+# Testing With Enzyme and Jest
+$ yarn test
+- jest with any JS
+- Enzyme with only React
+
+# Vocab
+Jest
+Enzyme
+Shallow Render
+debug()
+props()
+
+# set up
+$ yarn add -D enzyme react-test-renderer enzyme-adapter-react-16
+```javascript
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import  Adapter  from 'enzyme-adapter-react-16'
+
+
+Enzyme.configure({ adapter: new Adapter()})
+```
+Describe
+It
+Expect
+
+# __tests__
+    directories in both components and pages that jest and enzyme look for to run test
+# Imports and configuration
+    Import ... from ...
+    Enzyme.configure({ adapter: new Adapter()})
+# Arrange Act Assert
+    ex shallow
+        find
+        expect
+# Selector syntax
+https://enzymejs.github.io/enzyme/docs/api/selector.html
+# Assertion Syntax 
+expect(<componentVariable>.<elementQueryMethod>()).<matcher>(<expectedValue>)
+    expect(<actualThing>).<matcher>(<expectedValue>)
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import  Adapter  from 'enzyme-adapter-react-16'
+import App from './App'
+import Home from './pages/Home'
+import CatIndex from './pages/CatIndex'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new Adapter()})
+
+describe('When the app renders', ()=>{
+  it('displays a header and a footer',()=>{
+
+    //arrange
+    const renderedApp = shallow(<App/>)
+    //act
+    const renderedHeader = renderedApp.find("Header")
+    const renderedFooter = renderedApp.find("Footer")
+    //assert
+    expect(renderedHeader.length).toEqual(1)
+    expect(renderedFooter.length).toEqual(1)
+  })
+  it('provides a route"/" to the home component',()=>{
+     //arrange
+     const renderedApp = shallow(<App/>)
+     //act
+     const renderedHomeRoute = renderedApp.find('[path="/"]')
+    //  console.log("rendered home route debug", renderedHomeRoute.debug())
+    //  console.log("rendered home route props", renderedHomeRoute.props())
+    //assert
+    expect(renderedHomeRoute.props().component).toEqual(Home)
+  })
+  it('provides a route "/catindex" to the CatIndex component',()=>{
+    //arrange
+    const renderedApp = shallow(<App/>)
+    //act
+    const renderedCatIndexRoute = renderedApp.find('[path="/catindex"]')
+   //assert
+   expect(renderedCatIndexRoute.props().component).toEqual(CatIndex)
+ })
+})
