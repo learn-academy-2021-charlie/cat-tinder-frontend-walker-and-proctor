5fc687c2df57566e7242dbf4d64f13119abae9f7
sjproctor <sjproctor950@gmail.com>
2021-08-23 18:17:20 -0700

    removes boilerplate logos

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -6,7 +6,7 @@ class NotFound extends Component {
     return (
       <div className="page-body">
         <h3>Ooops, no cats here!</h3>
-        <img src={ notCat } alt="little dog in a knit hat" className="cat-friend" />
+        <img src={notCat} alt="little dog in a knit hat" className="cat-friend" />
       </div>
     )
   }
